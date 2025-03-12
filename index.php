7ccd6adbf0d1d544ffea893154cbd0c5865a6c54
Weslley <72828215+w-rosario@users.noreply.github.com>
2025-03-11 21:39:29 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+echo "<strong>Olá mundo!</strong>";
+
+$nome = "<br>Teste ";
+
+echo "<hr>";
+
+echo $nome;
+$nome = 100;
+echo $nome;
+echo "<br><br>Meu nome é $nome";
+
+echo "<hr>";
+for ($i=0;$i<10;$i++) {
+    echo $i," ";
+}
